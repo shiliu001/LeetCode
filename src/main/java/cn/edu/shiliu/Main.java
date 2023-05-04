8cfbd42cdc4886f4ec11b5c954f06335f635126a
shiliu <319227835@qq.com>
2023-05-04 14:19:37 +0800

    项目初始化

---
+++ src/main/java/cn/edu/shiliu/Main.java
@@ -0,0 +1,13 @@
+package cn.edu.shiliu;
+
+/**
+ * @Author shiliu
+ * @Date 2023/5/4 14:16
+ * @Description: ${DEC}
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
